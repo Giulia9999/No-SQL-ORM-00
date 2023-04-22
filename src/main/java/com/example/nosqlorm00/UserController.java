09c8404e4a6ec813e45f784f604cebea55322d82
Giulia <giuliac35@gmail.com>
2023-04-22 15:48:53 +0200

    Create CRUD operations inside UserController class

--- src/main/java/com/example/nosqlorm00/UserController.java
+++ src/main/java/com/example/nosqlorm00/UserController.java
@@ -1,10 +1,47 @@
 package com.example.nosqlorm00;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
 
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable String id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }else {
+            throw new Exception();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id,@RequestBody User user) throws Exception {
+        Optional<User> userId = userRepository.findById(id);
+        if(userId.isPresent()){
+            return userRepository.save(user);
+        }else {
+            throw new Exception();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id){
+        userRepository.deleteById(id);
+    }
 }
