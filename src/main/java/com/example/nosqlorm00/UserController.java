a8c3201e4625ed80613e9e66fed90e4ca49023c2
Giulia <giuliac35@gmail.com>
2023-04-22 15:37:58 +0200

    Create UserController class

---
+++ src/main/java/com/example/nosqlorm00/UserController.java
@@ -0,0 +1,10 @@
+package com.example.nosqlorm00;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
