43e64e187d43dd1ad79b394c8faa8c80a90e14ad
Giulia <giuliac35@gmail.com>
2023-04-22 15:32:42 +0200

    Create User repo interface

--- src/main/java/com/example/nosqlorm00/User.java
+++ src/main/java/com/example/nosqlorm00/User.java
@@ -1,6 +1,7 @@
 package com.example.nosqlorm00;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -10,5 +11,6 @@ public class User {
     private String id;
     private String name;
     private String lastname;
+    @Indexed(unique = true)
     private String email;
 }
---
+++ src/main/java/com/example/nosqlorm00/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.nosqlorm00;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
